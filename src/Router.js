22196f03ff8096f10cc0a327fb5345a10cbeeb82
김한솔 <gimhansol@gimhansol-ui-MacBookAir.local>
2023-02-06 18:22:57 +0900

    리뷰반영

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
+import Product from './pages/ProductList/Product/Product';
+import ProductDetail from './pages/ProductDetail/ProductDetail';
+import ProductList from './pages/ProductList/ProductList';
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/product-detail" element={<ProductDetail />} />
+        <Route path="/product-list" element={<ProductList />} />
       </Routes>
     </BrowserRouter>
   );
