649dbbc814442350c31c729dfc025fea64a5f8bc
soomminglee <soomming.lee@gmail.com>
2023-02-12 22:58:54 +0900

    "여러개의 상수 데이터 한 폴더로 구성"

---
+++ src/pages/ProductList/Data/FilterData.js
@@ -0,0 +1,72 @@
+export const FilterData = [
+  {
+    id: 1,
+    category: '아로마',
+    subcategory: [
+      {
+        id: 1,
+        title: '모든 향',
+      },
+      {
+        id: 2,
+        title: '우드',
+      },
+      {
+        id: 3,
+        title: '시트러스',
+      },
+      {
+        id: 4,
+        title: '스모키',
+      },
+      {
+        id: 5,
+        title: '허브',
+      },
+      {
+        id: 6,
+        title: '흙내음',
+      },
+    ],
+  },
+  {
+    id: 2,
+    category: '가격 범위',
+    subcategory: [
+      {
+        id: 1,
+        title: '모든 가격',
+      },
+      {
+        id: 2,
+        title: '￦42500 - ￦85000',
+      },
+      {
+        id: 3,
+        title: '￦85000 - ￦170000',
+      },
+    ],
+  },
+  {
+    id: 3,
+    category: '제형 타입',
+    subcategory: [
+      {
+        id: 1,
+        title: '모든 제형',
+      },
+      {
+        id: 2,
+        title: '밤',
+      },
+      {
+        id: 3,
+        title: '젤',
+      },
+      {
+        id: 4,
+        title: '크림',
+      },
+    ],
+  },
+];
