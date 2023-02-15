96928655e46c8828a98414cd6fca2b882f40aa85
laralina <laralina@laralinaui-MacBook-Pro.local>
2023-02-15 15:35:10 +0900

    Modified: 라우터 수정

--- src/Router.js
+++ src/Router.js
@@ -1,28 +1,25 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Nav from './components/Nav/Nav';
-import Footer from './components/Footer/Footer';
+import { MenuProvider } from './components/Nav/MenuModal/Hide';
 import Home from './pages/Home/Home';
+import Nav from './components/Nav/Nav';
 import Product from './pages/ProductList/Product/Product';
 import ProductDetail from './pages/ProductDetail/ProductDetail';
 import ProductList from './pages/ProductList/ProductList';
-// import Login from './pages/Login/Login';
-import Payment from './pages/Payment/Payment';
 
 function Router() {
   return (
-    <BrowserRouter>
-      <Nav />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/product" element={<Product />} />
-        <Route path="/productdetail/:id" element={<ProductDetail />} />
-        <Route path="/product-list" element={<ProductList />} />
-        {/* <Route path="/log-in" element={<Login />} /> */}
-        <Route path="payment" element={<Payment />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+    <MenuProvider>
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/product-detail" element={<ProductDetail />} />
+          <Route path="/product-list" element={<ProductList />} />
+        </Routes>
+      </BrowserRouter>
+    </MenuProvider>
   );
 }
 
